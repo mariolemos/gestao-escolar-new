ddb408ffeff95c30ccd6e65ce6785031f40bb19c
wagner lemos <wagner.lemos94@hotmail.com>
2026-07-04 13:34:03 -0300

    remove globalExceptionHandler

--- src/main/java/br/com/mariolemos/gestao_escolar/exception/ApiExceptionHandler.java
+++ src/main/java/br/com/mariolemos/gestao_escolar/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package br.com.mariolemos.gestao_escolar.exception;
 
+import br.com.digidata.crud.exception.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -7,6 +9,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +46,107 @@ public class ApiExceptionHandler {
         erroResponse.setMensagem(ex.getMessage());
         return new ResponseEntity<>(erroResponse, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorResponse> handleNotFound(
+            ResourceNotFoundException ex,
+            HttpServletRequest request
+    ){
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.NOT_FOUND.value(),
+                        HttpStatus.NOT_FOUND.name(),
+                        ex.getMessage(),
+                        request.getRequestURI(),
+                        Collections.emptyList()
+                );
+
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(response);
+    }
+
+
+
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity<ErrorResponse> handleBusiness(
+            BusinessException ex,
+            HttpServletRequest request
+    ){
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.BAD_REQUEST.value(),
+                        HttpStatus.BAD_REQUEST.name(),
+                        ex.getMessage(),
+                        request.getRequestURI(),
+                        Collections.emptyList()
+                );
+
+
+        return ResponseEntity
+                .badRequest()
+                .body(response);
+    }
+
+
+
+
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity<ErrorResponse> handleUnauthorized(
+            UnauthorizedException ex,
+            HttpServletRequest request
+    ){
+
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.UNAUTHORIZED.value(),
+                        HttpStatus.UNAUTHORIZED.name(),
+                        ex.getMessage(),
+                        request.getRequestURI(),
+                        Collections.emptyList()
+                );
+
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(response);
+
+    }
+
+
+
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneric(
+            Exception ex,
+            HttpServletRequest request
+    ){
+
+
+        ErrorResponse response =
+                new ErrorResponse(
+                        LocalDateTime.now(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                        HttpStatus.INTERNAL_SERVER_ERROR.name(),
+                        "Erro interno no servidor",
+                        request.getRequestURI(),
+                        Arrays.asList(ex.getMessage())
+                );
+
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(response);
+
+    }
+
 }
 
 
